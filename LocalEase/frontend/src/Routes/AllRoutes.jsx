f08dad7f6cbddc557afa6b3c9d38475c950d764b
shreshthkr <shreshthkumar6@gmail.com>
2023-03-29 21:42:49 +0530

    added sidebar

--- LocalEase/frontend/src/Routes/AllRoutes.jsx
+++ LocalEase/frontend/src/Routes/AllRoutes.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import {Routes, Route} from "react-router-dom";
+import { Checkout } from '../Pages/Checkout';
+import { DealsPage } from '../Pages/DealsPage/DealsPage';
+import HomePage from '../Pages/HomePage';
+import { NightDeals } from '../Pages/NightDeal/NightDeals';
 
 export const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/nightdeals" element={<NightDeals />} />
+      <Route path="/deals" element={<DealsPage />} />
+    </Routes>
   )
 }
