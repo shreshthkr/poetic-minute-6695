2edccc101424016bf8353e56d025960b7753a7c8
Kamal Gupta <agdigitalsmith@gmail.com>
2023-03-30 21:50:07 +0530

    add

--- LocalEase/frontend/src/index.js
+++ LocalEase/frontend/src/index.js
@@ -11,24 +11,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
-  <BrowserRouter>
-  
-    <App />
-  
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>
-
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+)
 reportWebVitals();
