824d2e5ff951d8a57ac579d91068f6000cd5b399
Kamal Gupta <agdigitalsmith@gmail.com>
2023-04-02 19:16:42 +0530

    add

--- LocalEase/backend/routes/product.router.js
+++ LocalEase/backend/routes/product.router.js
@@ -1,48 +1,40 @@
-  const mongoose = require("mongoose");
-  const express = require("express");
-  const productRouter = express.Router();
-  const {ProductModel} = require("../model/product.model");
-  const {authentication}=require("../middleware/admin.middleware")
- 
+const mongoose = require("mongoose");
+const express = require("express");
+const productRouter = express.Router();
+const { ProductModel } = require("../model/product.model");
+const { authentication } = require("../middleware/admin.middleware");
+const { cloudinary } = require("../utils/cloudinary");
 
-  productRouter.post("/addproduct" ,authentication, async(req , res) => {
-      try {
-           const product = new ProductModel(req.body);
-           await product.save();
-           res.status(200).send({"msg" : " Product data successfully added"})
-      }
 
-      catch(err) {
-           res.send({"msg" : err.message})
-      }
-  })
-
-  productRouter.get("/" , async(req , res) => {
+productRouter.post("/addproduct", authentication, async (req, res) => {
      try {
-          if(req.query.location == "Aerocity"){
-               const product = await ProductModel.find({category:"Aerocity"});
-               res.status(200).send(product);
-          }
-          if(req.query.location == "connaughtplace"){
-               const product = await ProductModel.find({category:"Connaught Place"});
-               res.status(200).send(product);
-          }
-          if(req.query.location == "rajourigarden"){
-               const product = await ProductModel.find({category:"Rajouri Garden Main Market, Rajouri Garden"});
-               res.status(200).send(product);
-          }
+          const { adminID, url, img_src, rating_img_src, rating, heading, category, value_2, price, tag, field } = req.body
+          if (img_src) {
+               const uploadRes = await cloudinary.uploader.upload(img_src, {
+                    upload_preset: "localEase_Image"
+               })
+               if (uploadRes) {
+                    const product = new ProductModel({ adminID, url, img_src: uploadRes, rating_img_src, rating, heading, category, value_2, price, tag, field });
+                    await product.save();
+                    res.status(200).send({ "msg": " Product data successfully added" })
 
-          if(req.query.location == "punjabibagh"){
-               const product = await ProductModel.find({category:"Club Road Market, Punjabi Bagh West"});
+               }
+          }
+     }
+     catch (err) {
+          res.send({ "msg": err.message })
+     }
+})
+
+productRouter.get("/", async (req, res) => {
+     try {
+          const category = req.query.category
+          console.log(category)
+          if (category) {
+               const product = await ProductModel.find({ category: category });
                res.status(200).send(product);
           }
-
-          if(req.query.location == "hkv"){
-               const product = await ProductModel.find({category:"HKV"});
-               res.status(200).send(product);
-          }
-
-          if((req.query.min) && (req.query.max) ){
+          if ((req.query.min) && (req.query.max)) {
                const product = await ProductModel.find({ price: { $gte: Number(req.query.min), $lte: Number(req.query.max) } });
                res.status(200).send(product);
           }
@@ -51,38 +43,38 @@
           res.status(200).send(product);
      }
 
-     catch(err) {
-          res.send({"msg" : err.message})
+     catch (err) {
+          res.send({ "msg": err.message })
      }
- })
+})
 
- productRouter.delete("/delete/:productID" , async(req , res) => {
-    const {productID} = req.params;
-    console.log(productID);
+productRouter.delete("/delete/:productID", async (req, res) => {
+     const { productID } = req.params;
+     console.log(productID);
      try {
-          const product = await ProductModel.findByIdAndDelete({_id : productID});
-          res.status(200).send({"msg" : "product deleted successfully"});
+          const product = await ProductModel.findByIdAndDelete({ _id: productID });
+          res.status(200).send({ "msg": "product deleted successfully" });
      }
 
-     catch(err) {
-          res.send({"msg" : err.message})
+     catch (err) {
+          res.send({ "msg": err.message })
      }
- })
+})
 
- productRouter.patch("/update/:productID" , async(req , res) => {
-     const {productID} = req.params;
-      try {
-           const product = await ProductModel.findByIdAndUpdate({_id : productID} , req.body);
-           res.status(200).send({"msg" : "Product Updated successfully"});
-      }
- 
-      catch(err) {
-           res.send({"msg" : err.message})
-      }
-  })
-  
+productRouter.patch("/update/:productID", async (req, res) => {
+     const { productID } = req.params;
+     try {
+          const product = await ProductModel.findByIdAndUpdate({ _id: productID }, req.body);
+          res.status(200).send({ "msg": "Product Updated successfully" });
+     }
 
-    module.exports={productRouter}
+     catch (err) {
+          res.send({ "msg": err.message })
+     }
+})
+
+
+module.exports = { productRouter }
 
 
 
